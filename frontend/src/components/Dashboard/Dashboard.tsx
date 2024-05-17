3e50b161e77c4f6d051024ecb7923d23e77d5459
kauasduarte <kauaduarte@gmail.com>
2024-05-16 21:24:43 -0300

    refactor: Update frontend dependencies and add React Select library

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect, ChangeEvent } from 'react';
 import { Link } from "react-router-dom";
 import styles from "./Dashboard.module.css"
 
+import ptBR from 'date-fns/locale/pt-BR';
+import { format, addDays } from 'date-fns';
+
 interface Dashboard {
   id: number;
   titulo: string;
@@ -13,7 +16,26 @@ interface Dashboard {
 
 export function Dashboard() {
   
+  const [tarefa, setTarefa] = useState<Dashboard[]>([]);
+  const [loading, setLoading] = useState(true);
+  // const club_id = Cookies.get('club_id');
 
+  const tarefasPorData = {};
+    tarefa.forEach(tarefas => {
+        const prazoTarefa = format(addDays(new Date(tarefas.prazo), 1), "dd 'de' MMMM 'de' yyyy", { locale: ptBR });
+        if (!tarefasPorData[prazoTarefa]) {
+            tarefasPorData[prazoTarefa] = [];
+        }
+        tarefasPorData[prazoTarefa].push(tarefas);
+    });
+
+     // Função para formatar a descrição com quebra de linha a cada 60 caracteres
+     const formatarDescricao = (descricao: string) => {
+      if (descricao.length > 60) {
+          return descricao.match(/.{1,60}/g).join('\n');
+      }
+      return descricao;
+  };
 
     return (
         <>
@@ -31,6 +53,37 @@ export function Dashboard() {
               </div>
               <hr style={{ borderTop: '2px solid gray', width: '100%' }} />
             </div>
+
+            {loading ? (
+                    <div>Carregando...</div>
+                ) : (
+                    Object.keys(tarefasPorData).map(data => (
+                        <div key={data}>
+                            <div className="col">
+                                <span className="mt-4">{data}</span>
+                            </div>
+
+                            {tarefasPorData[data].map((tarefas) => (
+                                <Link to={`/editreunion/${tarefas.id}`} className={`nav-link list-group-flush mt-4 ${styles.customReunion}`} key={reunioes.id}>
+                                    <div className="d-flex flex-row mt-4 mb-4">
+                                        <div className="list-group-item">
+                                            <span className="material-symbols-outlined" style={{ color: '#5b6b77' }}>connect_without_contact</span>
+                                            <div className="row d-flex">
+                                                <div className="col-md-12 mt-1">
+                                                    <span className="d-block">{tarefas.titulo}</span>
+                                                    <span style={{ color: '#5b6b77' }}>{formatarDescricao(tarefas.descricao)}</span>
+                                                </div>
+                                            </div>
+                                        </div>            
+                                    </div> 
+                                </Link> 
+                            ))}
+                        </div>
+                    ))
+                )}
+
+
+
           </div>
 
         </>
---
+++ frontend/src/components/Tarefas/CriarTarefas.tsx
@@ -0,0 +1,248 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Cookies from 'js-cookie';
+import { FormEvent, OptionType, CustomMultiValueProps } from '../types';
+
+import styles from '../Dashboard/Dashboard.module.css';
+
+import Select, { MultiValue } from 'react-select';
+
+interface Integrante {
+  name: string;
+  last_name: string;
+}
+
+interface Tarefa {
+  titulo?: string;
+  descricao?: string;
+  documento?: string;
+  status?: string;
+  data_publicacao?: string;
+  prazo?: string;
+  entrega?: string;
+}
+
+export function CreateTarefa() {
+    //salvar
+    const [tarefa, setTarefa] = useState<Tarefa>({});
+    const [, setStatus] = useState<string>('');
+    
+    // const user_id = Cookies.get('user_id');
+    // const club_id = Cookies.get('club_id');
+
+    const [integrantes, setIntegrantes] = useState<Integrante[]>([]);
+    const [, setLoading] = useState<boolean>(true);
+
+    const [selectedOptions, setSelectedOptions] = useState<OptionType[]>([]);
+  
+    async function gravar(e: FormEvent) {
+      e.preventDefault();
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+  
+      try {
+        const participantsArray = selectedOptions.map(option => option.value); // Extrai os valores dos participantes
+        await axios.post(
+          'http://127.0.0.1:8000/api/tarefa/create',
+          {
+            titulo: tarefa.titulo,
+            descricao: tarefa.descricao,
+            documento: tarefa.documento,
+            status: tarefa.status,
+            data_publicacao: tarefa.data_publicacao,
+            prazo: tarefa.prazo,
+            entrega: tarefa.entrega,
+            // club_id: club_id,
+            // user_id: user_id,
+            participants: participantsArray,
+            
+          },
+          config
+        );
+  
+        setStatus('Tarefa cadastrada com sucesso!');
+        alert('Tarefa cadastrada com sucesso!');
+        setTarefa({});
+        setSelectedOptions([]);
+      } catch (error) {
+        setStatus(`Falha: ${error}`);
+        alert(`Falha: ${error}`);
+        console.log(error);
+      }
+    }
+
+    // useEffect(() => {
+    //     async function Profile() {
+    //       try {
+    //         const response = await axios.get(`http://127.0.0.1:8000/api/clubIntegrantes/getClubIntegrantesWithUser/${club_id}`);
+    //         setIntegrantes(response.data);
+    //         setLoading(false);
+    //       } catch (error) {
+    //         console.error(error);
+    //       }
+    //     }
+    
+    //     Profile();
+    //   }, [club_id]);
+
+    //select    
+    const options = integrantes.map((integrante) => ({
+        value: `${integrante.name} ${integrante.last_name}`,
+        label: `${integrante.name} ${integrante.last_name}`,
+      }));
+
+    const handleSelectChange = (selectedValues: MultiValue<OptionType>) => {
+      setSelectedOptions([...selectedValues] || []);
+    };
+
+    const removeOption = (optionToRemove: OptionType) => {
+        const updatedOptions = selectedOptions.filter((option) => option !== optionToRemove);
+        setSelectedOptions(updatedOptions);
+    };
+
+    const customMultiValue = (props: CustomMultiValueProps) => (
+        <div className={`custom-multi-value ${styles.multiValue}`}>
+          {props.children}
+          <span className="remove" onClick={() => removeOption(props.data)}>×</span>
+        </div>
+    );
+
+    return(
+        <div className="container">
+            <b>Criar reunião</b>
+
+            <form className="mt-4" onSubmit={gravar}>
+                <div className="form-group mt-4">
+                    <label>Título</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      name="titulo" 
+                      placeholder="Título" 
+                      value={reunion.titulo || ''}
+                      onChange={(e) => setReunion({ ...reunion, titulo: e.target.value })}
+                      maxLength={60} 
+                      required/>
+                </div>
+                
+                <div className="form-group mt-4">
+                    <label>Descrição</label>
+                    <textarea 
+                        className="form-control" 
+                        name="descricao"
+                        placeholder="Descrição" 
+                        rows={4} 
+                        maxLength={255}
+                        style={{ resize: 'none' }}
+                        value={reunion.descricao || ''}
+                        onChange={(e) => setReunion({ ...reunion, descricao: e.target.value })}
+                        required
+                    />
+                </div>
+
+                <div className="form-group mt-4">
+                    <label>Link</label>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      name="link" 
+                      placeholder="Link" 
+                      value={reunion.link || ''}
+                      onChange={(e) => setReunion({ ...reunion, link: e.target.value })} 
+                      required
+                    />
+                </div>
+
+                <div className="form-group mt-4">
+                    <div className="mt-2">
+                        <p>Participantes:</p>
+                    </div>
+                
+                    <Select
+                        isMulti
+                        options={options}
+                        value={selectedOptions}
+                        name="participants_name"
+                        onChange={handleSelectChange}
+                        components={{ MultiValue: customMultiValue }}
+                        required
+                    />              
+                </div>
+
+                <div className="row mt-4">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputData">Data</label>
+                        <input 
+                          type="date" 
+                          className="form-control" 
+                          id="inputHora" 
+                          name="data_reuniao" 
+                          placeholder="Data" 
+                          value={reunion.data_reuniao || ''}
+                          onChange={(e) => setReunion({ ...reunion, data_reuniao: e.target.value })}
+                          required
+                        />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputHora">Hora</label>
+                        <input 
+                          type="time" 
+                          className="form-control" 
+                          id="inputHora" 
+                          name="hora_reuniao" 
+                          placeholder="Hora" 
+                          value={reunion.hora_reuniao || ''}
+                          onChange={(e) => setReunion({ ...reunion, hora_reuniao: e.target.value })} 
+                          required
+                        />
+                    </div>
+                </div>   
+
+                <div className="row mt-4">
+                    <div className="form-group col-md-6">
+                        <label>Livro</label>
+                        <input 
+                          type="text" 
+                          className="form-control" 
+                          name="livro" 
+                          placeholder="Livro" 
+                          value={reunion.livro || ''}
+                          onChange={(e) => setReunion({ ...reunion, livro: e.target.value })}
+                          maxLength={60}
+                        />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label>Autor   </label>
+                        <input 
+                          type="text" 
+                          className="form-control" 
+                          name="autor" 
+                          placeholder="autor" 
+                          value={reunion.autor || ''}
+                          onChange={(e) => setReunion({ ...reunion, autor: e.target.value })}
+                          maxLength={60}
+                        />
+                    </div>
+                </div>
+
+                <div className="form-group mt-4">         
+                    <button type="submit" className="mt-2"> 
+                      Salvar 
+                    </button>
+
+                    <Link to={"/reunion"}>
+                        <button type="button" className="mt-4">
+                            Voltar
+                        </button>
+                    </Link>
+                </div>
+
+            </form>
+        </div>
+    )
+}
