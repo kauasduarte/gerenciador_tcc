e51bbf25cfcfff4d6def692d23bf53657c0a1c4c
André Nery <88936386+andreneery@users.noreply.github.com>
2024-05-17 21:46:50 -0300

    criando tabelas de projeto e tarefas e api

---
+++ backend/database/migrations/2024_05_18_002456_create_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descricao')->nullable();
+            $table->binary('documento')->nullable();
+            $table->string('documento_nome')->nullable();
+            $table->boolean('status')->nullable(false);
+            $table->date('data_publicacao');
+            $table->date('data_entrega');
+            $table->foreignId('projeto_id')->constrained('table_projeto')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\UsersController;
+use App\Http\Controllers\TasksController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -11,3 +12,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [UsersController::class, 'register']);
 Route::post('/login', [UsersController::class, 'login']);
+
+
+//rotas de task 
+Route::post('/tarefa/create', [UsersController::class,'createTask']);
+Route::get('/tarefa/getAllTasks/{projeto_id}', [UsersController::class,'getAllTasks']);
+
