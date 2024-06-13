ee3a16372ab3c899bb4a998b93a5045634e51704
kauasduarte <kauaduarte@gmail.com>
2024-06-13 00:10:06 -0300

    Refactor Membros component and fetch members from API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,9 +33,6 @@ function App() {
     </>
   );
 
-  // return (
-  //   <RoutesLogin />
-  // )
 }
 
 export default App;
--- frontend/src/components/Membros/Membros.tsx
+++ frontend/src/components/Membros/Membros.tsx
@@ -4,12 +4,15 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 
 export function Membros() {
-  const [members, setMembers] = useState([]);
+  const [members, setMembers] = useState([]);  
+  const projeto_id = Cookies.get('projeto_id');
+  const tipo_usuario = Cookies.get('role');
 
+  
   useEffect(() => {
     async function fetchMembers() {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/user_project/getProjectWithUsers/1');
+        const response = await axios.get(`http://127.0.0.1:8000/api/user_project/getProjectWithUsers/${projeto_id}`);
         setMembers(response.data); // Assume-se que a resposta é um array diretamente
       } catch (error) {
         console.error('Erro ao buscar membros:', error);
@@ -19,7 +22,6 @@ export function Membros() {
     fetchMembers();
   }, []);
 
-  const projeto_id = Cookies.get('user_id');
 
   return (
     <>
@@ -32,12 +34,26 @@ export function Membros() {
           <hr style={{ borderTop: '2px solid gray', width: '100%' }} />
         </header>
         <main className={`${styles.membros}`}>
-          {members.map(member => (
-            <section key={member.user_id}>
-              <h3>{member.name}</h3>
-              <p>{member.tipo_usuario}</p>
-              <button>Remover</button>
-            </section>
+        {members.map(member => (
+  <section key={member.user_id}>
+    <h3>{member.name}</h3>
+    <p>{member.tipo_usuario}</p>
+    {tipo_usuario === 'orientador' || tipo_usuario === 'coordenador' ? (
+      <button onClick={async () => {
+        if (window.confirm('Tem certeza que deseja remover este usuário?')) {
+          try {
+            const response = await axios.delete(`http://127.0.0.1:8000/api/user_project/deleteUser/${member.user_id}/${projeto_id}`);
+            console.log(response.data);
+            // You might want to update the members list here
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }}>
+        Remover
+      </button>
+    ) : null}
+  </section>
           ))}
         </main>
       </div>
